39d8f5c30ddd73989c6eb99811ba051591caa26a
Polina Nenchev <polina.mihova@yahoo.com>
2016-06-09 15:13:15 -0700

    add font to tips/suggestions

--- app/src/main/java/com/mikeschen/www/fitnessapp/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mikeschen.www.fitnessapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,7 +10,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     private ListView mDrawerList;
@@ -22,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface myTypeFace = Typeface.createFromAsset(getAssets(), "fonts/PTN77F.ttf");
+        TextView myTextview = (TextView) findViewById(R.id.tipsTextView);
+        myTextview.setTypeface(myTypeFace);
+
         mDrawerList = (ListView) findViewById(R.id.navList);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mActivityTitle = getTitle().toString();
