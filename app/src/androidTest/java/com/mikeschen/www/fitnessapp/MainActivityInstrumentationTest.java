c14d6a837dae899143d6b775870d9bfa8c674480
vrdlc <virgilioramon.delacruz@gmail.com>
2016-06-13 16:56:42 -0700

    add two instrumentation tests to Main Activity

--- app/src/androidTest/java/com/mikeschen/www/fitnessapp/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/mikeschen/www/fitnessapp/MainActivityInstrumentationTest.java
@@ -6,7 +6,9 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -21,5 +23,16 @@ public class MainActivityInstrumentationTest {
     @Test
     public void validateTextView() {
         onView(withId(R.id.tipsTextView)).check(matches(withText("Tips/Suggestions")));
+    }
+
+    @Test
+    public void doNotClickMainButton() {
+        onView(withId(R.id.mainButton)).check(matches(withText("Calories Burned: 200")));
+    }
+
+    @Test
+    public void clickMainButton() {
+        onView(withId(R.id.mainButton)).perform(click())
+                .check(matches(withText("Steps Taken: 75")));
     }
 }
