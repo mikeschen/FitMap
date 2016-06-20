4960d8c91641cfaf78ac0c5baa5666e6b0fa4bea
Polina Nenchev <polina.mihova@yahoo.com>
2016-06-20 10:06:19 -0700

    add app name and calligraphy to about activity

--- app/src/main/java/com/mikeschen/www/fitnessapp/AboutActivity.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/AboutActivity.java
@@ -1,28 +1,23 @@
 package com.mikeschen.www.fitnessapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
-import butterknife.Bind;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
 public class AboutActivity extends AppCompatActivity {
-    @Bind(R.id.aboutTextView) TextView mAboutTextView;
-    @Bind(R.id.benefitsTextView) TextView mBenefitsTextView;
-    @Bind(R.id.creationTextView) TextView mCreationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        //add background picture on the about page- use learn how to program -DONE
-        //create two strings to getText/setText to display the creation of the app and the benefits of passively working out
-        //look for a different picture to be set as a background
-        //try to write JUnit tests at list for the About page
+    }
 
-
-
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(context));
     }
 }
