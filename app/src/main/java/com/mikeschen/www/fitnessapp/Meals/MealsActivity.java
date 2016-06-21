1714baa3627158d6ed0c3096a6dd1602b488e30b
Polina Nenchev <polina.mihova@yahoo.com>
2016-06-21 11:44:42 -0700

    add search bar

--- app/src/main/java/com/mikeschen/www/fitnessapp/Meals/MealsActivity.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/Meals/MealsActivity.java
@@ -1,7 +1,14 @@
 package com.mikeschen.www.fitnessapp.Meals;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,10 +21,14 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MealsActivity extends AppCompatActivity {
-    @Bind(R.id.foodInputEditText) EditText mFoodInputEditText;
-    @Bind(R.id.totalCaloriesTextView) TextView mTotalCaloriesTextView;
-    @Bind(R.id.dailyCaloriesBurnedTextView) TextView mDailyCaloriesBurnedTextView;
-    @Bind(R.id.todaysDate) TextView mTodaysDate;
+    @Bind(R.id.foodInputEditText)
+    EditText mFoodInputEditText;
+    @Bind(R.id.totalCaloriesTextView)
+    TextView mTotalCaloriesTextView;
+    @Bind(R.id.dailyCaloriesBurnedTextView)
+    TextView mDailyCaloriesBurnedTextView;
+    @Bind(R.id.todaysDate)
+    TextView mTodaysDate;
 
 
     @Override
@@ -26,6 +37,7 @@ public class MealsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_meals);
 
         ButterKnife.bind(this);
+
 //        final Calendar c = Calendar.getInstance();
 //        char[] day = c.get(Calendar.DAY_OF_MONTH);
 //        int month = c.get(Calendar.MONTH);
@@ -33,8 +45,53 @@ public class MealsActivity extends AppCompatActivity {
 //        mTodaysDate.setText(day, month, year);
 
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat mdformat = new SimpleDateFormat("yyyy / MM / dd ");
+        SimpleDateFormat mdformat = new SimpleDateFormat("MM / dd / yyyy");
         String strDate = "Today's Date : " + mdformat.format(calendar.getTime());
         mTodaysDate.setText(strDate);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // hide action item
+                getSupportActionBar().setTitle("");
+            }
+        });
+        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                getSupportActionBar().setTitle("FitnessApp");
+                return false;
+            }
+        });
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String mFoodInputEditText) {
+                Intent intent = new Intent(MealsActivity.this, MealsSearchResultActivity.class);
+                intent.putExtra("mFoodInputEditText", mFoodInputEditText);
+                startActivity(intent);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
