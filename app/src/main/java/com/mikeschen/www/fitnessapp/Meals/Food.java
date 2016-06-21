e31c74b5c0f994233fb3861659dcbfa38d8ab695
Polina Nenchev <polina.mihova@yahoo.com>
2016-06-21 10:55:26 -0700

    construct nutritionix service and JSON objects

---
+++ app/src/main/java/com/mikeschen/www/fitnessapp/Meals/Food.java
@@ -0,0 +1,68 @@
+package com.mikeschen.www.fitnessapp.Meals;
+
+public class Food {
+    String itemId;
+    String itemName;
+    String brandName;
+    String itemDescription;
+    double calories;
+    double totalFat;
+    double servingsPerContainer;
+    double servingSizeQuantity;
+    String servingSizeUnit;
+    double servingWeightGrams;
+
+    public Food() {}
+    public Food(String itemId, String itemName, String brandName, String itemDescription, double calories, double totalFat, double servingsPerContainer, double servingSizeQuantity, String servingSizeUnit, double servingWeightGrams) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.brandName = brandName;
+        this.itemDescription = itemDescription;
+        this.calories = calories;
+        this.totalFat = totalFat;
+        this.servingsPerContainer = servingsPerContainer;
+        this.servingSizeQuantity = servingSizeQuantity;
+        this.servingSizeUnit = servingSizeUnit;
+        this.servingWeightGrams = servingWeightGrams;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public double getTotalFat() {
+        return totalFat;
+    }
+
+    public double getServingsPerContainer() {
+        return servingsPerContainer;
+    }
+
+    public double getServingSizeQuantity() {
+        return servingSizeQuantity;
+    }
+
+    public String getServingSizeUnit() {
+        return servingSizeUnit;
+    }
+
+    public double getServingWeightGrams() {
+        return servingWeightGrams;
+    }
+}
---
+++ app/src/main/java/com/mikeschen/www/fitnessapp/Meals/MealsPresenter.java
@@ -0,0 +1,10 @@
+package com.mikeschen.www.fitnessapp.Meals;
+
+/**
+ * Created by alexnenchev on 6/21/16.
+ */
+//public class MealsPresenter implements
+//    MealsInterface.Presenter,
+//        View.OnClickListener,
+//{
+//}
