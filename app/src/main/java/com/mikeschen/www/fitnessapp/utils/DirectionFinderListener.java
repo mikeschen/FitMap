bcd0a9e9255cd75c690fbddca4257783f823291c
Michael Schennum <mikeschenphoto@gmail.com>
2016-06-29 13:29:49 -0700

    adds dismiss for dialog

--- app/src/main/java/com/mikeschen/www/fitnessapp/utils/DirectionFinderListener.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/utils/DirectionFinderListener.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface DirectionFinderListener {
     void onDirectionFinderStart();
     void onDirectionFinderSuccess(List<Route> route);
+    void onDirectionFinderFail();
 }
