f715f02d14824f2e8e7ce5902bb6624395bb5d0c
Polina Nenchev <polina.mihova@yahoo.com>
2016-07-05 09:42:43 -0700

    add transition from RealStatsActvity to mainActivity

--- app/src/main/java/com/mikeschen/www/fitnessapp/simpleActivities/RealStatsActivity.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/simpleActivities/RealStatsActivity.java
@@ -4,17 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.mikeschen.www.fitnessapp.BaseActivity;
 import com.mikeschen.www.fitnessapp.R;
+import com.mikeschen.www.fitnessapp.main.TipPresenter;
 import com.mikeschen.www.fitnessapp.maps.MapsActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RealStatsActivity extends BaseActivity implements View.OnClickListener {
-    @Bind(R.id.suggestionButton) Button mSuggestionButton;
+    @Bind(R.id.suggestionButton)
+    Button mSuggestionButton;
+    @Bind(R.id.caloriesTextView)
+    TextView mCaloriesTextView;
+    @Bind(R.id.stepsTextView)
+    TextView mStepsTextView;
+    @Bind(R.id.dateTextView)
+    TextView mDateTextView;
 
+    private TipPresenter mTipPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,10 @@ public class RealStatsActivity extends BaseActivity implements View.OnClickListe
         ButterKnife.bind(this);
 
         mSuggestionButton.setOnClickListener(this);
+//        db.bindCalories();
+        //how to tell information from db to put on this page
+
+
 
     }
 
@@ -30,10 +44,20 @@ public class RealStatsActivity extends BaseActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case (R.id.suggestionButton):
-                Intent intent = new Intent (RealStatsActivity.this, MapsActivity.class);
+                Intent intent = new Intent(RealStatsActivity.this, MapsActivity.class);
                 startActivity(intent);
                 break;
 
         }
     }
 }
+
+
+//pull stuff from database to show steps and calories burned from yesterday
+
+
+
+
+
+
+
