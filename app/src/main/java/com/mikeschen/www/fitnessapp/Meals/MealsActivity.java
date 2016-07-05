af37175d74cb07afa71294f3d62a2bfba16c037f
Michael Schennum <mikeschenphoto@gmail.com>
2016-07-05 14:42:35 -0700

    adds xml

--- app/src/main/java/com/mikeschen/www/fitnessapp/Meals/MealsActivity.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/Meals/MealsActivity.java
@@ -13,7 +13,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
