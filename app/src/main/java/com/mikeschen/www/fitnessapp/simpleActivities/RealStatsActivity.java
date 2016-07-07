f430ef8d39a976bb7a1b9b293d74066673e367b5
Matt Rosanio <mmrosani@syr.edu>
2016-07-07 09:38:48 -0700

    fix issue with timer so notification arrives at midnight

--- app/src/main/java/com/mikeschen/www/fitnessapp/simpleActivities/RealStatsActivity.java
+++ app/src/main/java/com/mikeschen/www/fitnessapp/simpleActivities/RealStatsActivity.java
@@ -1,7 +1,6 @@
 package com.mikeschen.www.fitnessapp.simpleActivities;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
